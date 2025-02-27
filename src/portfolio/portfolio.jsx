653abd706edcf7ff18a85512d25b50c74ec84166
nraustin <nickademic@gmail.com>
2025-02-26 23:09:50 -0700

    Updated authenticated component to render Portfolio. Partially mocked out stock query.

--- src/portfolio/portfolio.jsx
+++ src/portfolio/portfolio.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 
-export function Portfolio() {
+export function Portfolio({username}) {
+  const [stockSymbol, setStockSymbol] = useState('');
+
+  function handleStockSearch() {
+    console.log(`Searching for ${stockSymbol}`);
+  }
+
   return (
     <main className="main-portfolio">
       <div className="portfolio-container">
-        <h2 className="portfolio-welcome-msg">Hi, Demo.</h2>
+        <h2 className="portfolio-welcome-msg">Hi, {username}.</h2>
         <p>
           Portfolio value: <strong>$<span id="portfolio-value">1000</span> USD</strong>
         </p>
@@ -13,8 +20,14 @@ export function Portfolio() {
       <div className="stockquery-container">
         <label htmlFor="stock-symbol">Find a stock</label>
         <div className="stockquery">
-          <input type="text" id="stock-symbol" placeholder="NVDA" />
-          <button type="submit" className="stockquery-button">Search</button>
+          <input type="text" 
+                 id="stock-symbol" 
+                 placeholder="NVDA" 
+                 value={stockSymbol}
+                 onChange={(e) => setStockSymbol(e.target.value)}/>
+          <button type="submit" 
+                  className="stockquery-button"
+                  onClick={handleStockSearch}>Search</button>
         </div>
       </div>
     </main>
