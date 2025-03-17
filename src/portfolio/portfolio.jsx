7fdb773c5a697364046574f103f9384acfce7164
nraustin <nickademic@gmail.com>
2025-03-17 16:16:03 -0600

    Connected Polygon API for symbol queries in index.js. Wrote async stock suggestion handler for queries in portfolio.jsx. Minor styling added to suggestions box.

--- src/portfolio/portfolio.jsx
+++ src/portfolio/portfolio.jsx
@@ -4,11 +4,38 @@ import { useNavigate } from 'react-router-dom';
 
 export function Portfolio({ username, portfolioValue  }) {
   const [stockSymbol, setStockSymbol] = useState('');
+  const [symbolSuggestions, setSymbolSuggestions] = useState([]);
   const navigate = useNavigate()
 
   React.useEffect(() => {
     localStorage.setItem('portfolioValue', portfolioValue);
   }, [portfolioValue]);
+
+  async function fetchStockSuggestions(query) {
+    if (!query.trim()) {
+      setSymbolSuggestions([]);
+      return;
+    }
+    try {
+      const response = await fetch(`/api/stocks?query=${query}`);
+      const data = await response.json();
+      setSymbolSuggestions(data);
+    } catch (err) {
+      console.err("Error fetching stock suggestions:", err);
+      setSymbolSuggestions([]);
+    }
+  }
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+    setStockSymbol(value);
+    fetchStockSuggestions(value); 
+  }
+
+  function handleStockSelect(symbol) {
+    setStockSymbol(symbol);
+    setSymbolSuggestions([]);
+  }
 
   function handleStockSearch() {
     if (stockSymbol.trim()) {
@@ -29,15 +56,26 @@ export function Portfolio({ username, portfolioValue  }) {
       <div className="stockquery-container">
         <label htmlFor="stock-symbol">Find a stock</label>
         <div className="stockquery">
-          <input type="text" 
-                 id="stock-symbol" 
-                 placeholder="NVDA" 
-                 value={stockSymbol}
-                 onChange={(e) => setStockSymbol(e.target.value)}/>
-          <button type="submit" 
-                  className="stockquery-button"
-                  onClick={handleStockSearch}>Search</button>
-        </div>
+          <div className='stockquery-field'>
+            <input type="text" 
+                  id="stock-symbol" 
+                  placeholder="NVDA" 
+                  value={stockSymbol}
+                  onChange={handleInputChange}/>
+            <button type="submit" 
+                    className="stockquery-button"
+                    onClick={handleStockSearch}>Search</button>
+          </div>
+          {symbolSuggestions.length > 0 && (
+          <ul className="stock-suggestions">
+            {symbolSuggestions.map(stock => (
+              <li key={stock.ticker} onClick={() => handleStockSelect(stock.ticker)}>
+                {stock.ticker}-{stock.name}
+              </li>
+            ))}
+          </ul>
+        )}
+        </div> 
       </div>
     </main>
   );
