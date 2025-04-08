8383cd707cc22d22547d3d34d0da100dfd8f384f
nraustin <nickademic@gmail.com>
2025-04-08 13:29:39 -0600

    Added missing message tracking for stock rooms

--- src/stock/chatNotifier.js
+++ src/stock/chatNotifier.js
@@ -1,37 +1,29 @@
 class ChatNotifier {
     constructor() {
         this.subscribers = [];
-        this.messages = [];
+        this.messages = {};
         this.ws = null;
-        
-        // this.placeholderUsers = ["John", "Hugo", "Charlie", "Jin"];
-        // this.randomMessages = [
-        //     "I feed on volatility",
-        //     "I had a GPU for breakfast.",
-        //     "This is so overhyped, c'mon guys",
-        //     "Why does this price action seem fake?",
-        //     "You couldn't lose your money faster even if you lit it on fire!",
-        // ]
-
-        // setInterval(() => {
-        //     this.sendRandomMessage();
-        // }, 3000);
+        this.currSymbol = null;
     }
 
     connect(symbol) {
         if(this.ws) {
             this.ws.close();
         }
+        this.currSymbol = symbol;
         const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
         const host = import.meta.env.DEV ? 'localhost:4000' : window.location.host;
         this.ws = new WebSocket(`${protocol}://${host}/chat?symbol=${symbol}`);
         this.ws.onmessage = (event) => {
             const message = JSON.parse(event.data);
-            this.messages.push(message);
-            if (this.messages.length > 5) {
-                this.messages.shift();
+            if(!this.messages[symbol]) {
+                this.messages[symbol] = [];
             }
-            this.subscribers.forEach(cb => cb([...this.messages]));
+            this.messages[symbol].push(message);
+            if (this.messages[symbol].length > 5) {
+                this.messages[symbol].shift();
+            }
+            this.subscribers.forEach(cb => cb([...this.messages[symbol]]));
         }
 
         this.ws.onclose = () => {
@@ -42,7 +34,8 @@ class ChatNotifier {
 
     subscribe(callback) {
         this.subscribers.push(callback);
-        callback(this.messages); 
+        const messages = this.messages[this.currSymbol] || [];
+        callback(messages); 
     }
 
     sendMessage(username, message) {
@@ -50,22 +43,7 @@ class ChatNotifier {
         if (this.ws && this.ws.readyState === WebSocket.OPEN) {
             this.ws.send(JSON.stringify(chatMsg));
         }
-        // const chatMessage = new ChatMessage(username, message);
-        // this.messages.push(chatMessage);
-
-        // if (this.messages.length > 5) {
-        //     this.messages.shift()
-        // }
-
-        // this.subscribers.forEach(callback => callback([...this.messages]));
     }
-
-    // sendRandomMessage() {
-    //     const randUser = this.placeholderUsers[Math.floor(Math.random()*this.placeholderUsers.length)];
-    //     const randMsg = this.randomMessages[Math.floor(Math.random()*this.randomMessages.length)];
-
-    //     this.sendMessage(randUser, randMsg);
-    // }
 }
 
 export const chatNotifier = new ChatNotifier();
